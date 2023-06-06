0d919ce52b3b80979128dff870d5603480eee937
旧忆 残梦 <125112916+Oldmemorie@users.noreply.github.com>
2023-06-06 12:20:48 +0800

    deploy

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -2,7 +2,7 @@ import type { HeadConfig } from 'vitepress';
 import { metaData } from './constants';
 
 export const head: HeadConfig[] = [
-  ['link', { rel: 'icon', href: '/logo.png' }],
+  ['link', { rel: 'icon', href: '/favicon.ico' }],
   ['meta', { name: 'author', content: 'Oldmemorie' }],
   ['meta', { name: 'keywords', content: '查尔斯的知识库, 知识库, 博客, Oldmemorie' }],
 
