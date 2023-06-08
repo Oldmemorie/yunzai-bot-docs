0ae3a2fecea7be464c4b38cdb51c37652280e999
旧忆 残梦 <125112916+Oldmemorie@users.noreply.github.com>
2023-06-08 10:31:38 +0800

    deploy

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -3,7 +3,7 @@ import { metaData } from './constants';
 
 
 export const head: HeadConfig[] = [
-  ['link', { rel: 'icon', href: './yunzai-bot-docs/favicon.ico' }],
+  ['link', { rel: 'icon', href: './favicon.ico' }],
   ['meta', { name: 'author', content: 'Charles7c' }],
   ['meta', { name: 'keywords', content: '查尔斯的知识库, 知识库, 博客, Charles7c' }],
 
