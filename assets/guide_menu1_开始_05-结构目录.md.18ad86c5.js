9a788ea915cee5bb56074e9c5e7e812e3285dd4e
Oldmemorie <125112916+Oldmemorie@users.noreply.github.com>
2023-07-01 09:53:33 +0000

    Deploying to pages from @ Oldmemorie/yunzai-bot-docs@0929ad561caf7721933766a7703e6fde43ad7aef 🚀

---
+++ assets/guide_menu1_开始_05-结构目录.md.18ad86c5.js
@@ -0,0 +1,39 @@
+import{_ as c}from"./chunks/ArticleMetadata.a3e318aa.js";import{_ as t,o as e,c as i,J as r,E as d,D as y,G as u,C as l,a as _,V as m,H as b}from"./chunks/framework.be8cb041.js";const x=JSON.parse('{"title":"结构目录","description":"","frontmatter":{"title":"结构目录"},"headers":[],"relativePath":"guide/menu1/开始/05-结构目录.md","filePath":"guide/menu1/开始/05-结构目录.md","lastUpdated":1686403726000}'),g={name:"guide/menu1/开始/05-结构目录.md"},f=l("h1",{id:"yunzai-结构主要目录",tabindex:"-1"},[_("Yunzai 结构主要目录 "),l("a",{class:"header-anchor",href:"#yunzai-结构主要目录","aria-label":'Permalink to "Yunzai 结构主要目录"'},"​")],-1),h=m(`<blockquote><p>这里只显示主要结构，毕竟整个目录包括子文件夹太多了，不知道也没关系统, <s>绝对不是我写不出来</s></p></blockquote><hr><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark-dimmed vp-code-dark"><code><span class="line"><span style="color:#adbac7;">.Yunzai</span></span>
+<span class="line"><span style="color:#adbac7;"> ├─ .git</span></span>
+<span class="line"><span style="color:#adbac7;"> ├─ config //配置</span></span>
+<span class="line"><span style="color:#adbac7;"> │  ├─ config</span></span>
+<span class="line"><span style="color:#adbac7;"> │  │  ├─ bot.yaml //bot配置</span></span>
+<span class="line"><span style="color:#adbac7;"> │  │  ├─ group.yaml //群组配置</span></span>
+<span class="line"><span style="color:#adbac7;"> │  │  ├─ other.yaml //其他配置，比如主人设置，黑名单之类的</span></span>
+<span class="line"><span style="color:#adbac7;"> │  │  ├─ redis.yaml //redis配置，不用管</span></span>
+<span class="line"><span style="color:#adbac7;"> │  └─ default_config //作为config的备份文件，千万不要动</span></span>
+<span class="line"><span style="color:#adbac7;"> │</span></span>
+<span class="line"><span style="color:#adbac7;"> ├─ data //数据</span></span>
+<span class="line"><span style="color:#adbac7;"> │  ├─ face //群组表情存放位置</span></span>
+<span class="line"><span style="color:#adbac7;"> │  ├─ myscookie //cookie存放位置</span></span>
+<span class="line"><span style="color:#adbac7;"> │  └─ userdata //绑定uid存放位置</span></span>
+<span class="line"><span style="color:#adbac7;"> └─ plugins</span></span>
+<span class="line"><span style="color:#adbac7;">     ├─ example //js插件存放位置</span></span>
+<span class="line"><span style="color:#adbac7;">     ├─ genshin //自带插件，别再找不到米游社资讯推送了</span></span>
+<span class="line"><span style="color:#adbac7;">     ├─ other //小白勿动</span></span>
+<span class="line"><span style="color:#adbac7;">     ├─ system //同上</span></span>
+<span class="line"><span style="color:#adbac7;">     ├─ …… //其他插件依次类推</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">.Yunzai</span></span>
+<span class="line"><span style="color:#24292e;"> ├─ .git</span></span>
+<span class="line"><span style="color:#24292e;"> ├─ config //配置</span></span>
+<span class="line"><span style="color:#24292e;"> │  ├─ config</span></span>
+<span class="line"><span style="color:#24292e;"> │  │  ├─ bot.yaml //bot配置</span></span>
+<span class="line"><span style="color:#24292e;"> │  │  ├─ group.yaml //群组配置</span></span>
+<span class="line"><span style="color:#24292e;"> │  │  ├─ other.yaml //其他配置，比如主人设置，黑名单之类的</span></span>
+<span class="line"><span style="color:#24292e;"> │  │  ├─ redis.yaml //redis配置，不用管</span></span>
+<span class="line"><span style="color:#24292e;"> │  └─ default_config //作为config的备份文件，千万不要动</span></span>
+<span class="line"><span style="color:#24292e;"> │</span></span>
+<span class="line"><span style="color:#24292e;"> ├─ data //数据</span></span>
+<span class="line"><span style="color:#24292e;"> │  ├─ face //群组表情存放位置</span></span>
+<span class="line"><span style="color:#24292e;"> │  ├─ myscookie //cookie存放位置</span></span>
+<span class="line"><span style="color:#24292e;"> │  └─ userdata //绑定uid存放位置</span></span>
+<span class="line"><span style="color:#24292e;"> └─ plugins</span></span>
+<span class="line"><span style="color:#24292e;">     ├─ example //js插件存放位置</span></span>
+<span class="line"><span style="color:#24292e;">     ├─ genshin //自带插件，别再找不到米游社资讯推送了</span></span>
+<span class="line"><span style="color:#24292e;">     ├─ other //小白勿动</span></span>
+<span class="line"><span style="color:#24292e;">     ├─ system //同上</span></span>
+<span class="line"><span style="color:#24292e;">     ├─ …… //其他插件依次类推</span></span></code></pre></div>`,3);function k(s,C,v,V,N,T){const p=c,o=b("ClientOnly");return e(),i("div",null,[f,r(o,null,{default:d(()=>{var a,n;return[(((a=s.$frontmatter)==null?void 0:a.aside)??!0)&&(((n=s.$frontmatter)==null?void 0:n.showArticleMetadata)??!0)?(e(),y(p,{key:0,article:s.$frontmatter},null,8,["article"])):u("",!0)]}),_:1}),h])}const A=t(g,[["render",k]]);export{x as __pageData,A as default};
